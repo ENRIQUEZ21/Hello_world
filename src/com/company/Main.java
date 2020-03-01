1f30776ba082c3024207a3befafdcd330b7a8314
ENRIQUEZ21 <58740925+ENRIQUEZ21@users.noreply.github.com>
2020-03-01 11:09:40 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+        System.out.println("Bonjour le monde");
+        System.out.println(functionCalculs());
+        System.out.print(functionCalculsExp(6));
+    }
+    public static int functionCalculs() {
+        return 2;
+    }
+    public static int functionCalculsExp(double n) {
+        return (int) ((Math.exp(n) * n) + (n * n * Math.getExponent(n)));
+    }
+}
